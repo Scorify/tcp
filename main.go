775e13e76091a1fcc1f1ba919ed037baa6a1b663
1nv8rZim <msf9542@rit.edu>
2024-09-27 18:21:40 -0400

    implement tcp check

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
-package tcp
+package main
 
 import (
 	"context"
 	"encoding/json"
+	"fmt"
+	"net"
 )
 
 type Schema struct {
@@ -17,6 +19,15 @@ func Run(ctx context.Context, config string) error {
 	if err != nil {
 		return err
 	}
+
+	connStr := fmt.Sprintf("%s:%d", schema.Host, schema.Port)
+
+	dialer := &net.Dialer{}
+	conn, err := dialer.DialContext(ctx, "tcp", connStr)
+	if err != nil {
+		return fmt.Errorf("failed to connect to %q; %w", connStr, err)
+	}
+	defer conn.Close()
 
 	return nil
 }
