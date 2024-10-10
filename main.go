799d23bfda4a09256fae012f694afc7847d1b452
1nv8rZim <msf9542@rit.edu>
2024-10-10 19:05:34 -0400

    add `Validate` function

--- main.go
+++ main.go
@@ -5,11 +5,32 @@ import (
 	"encoding/json"
 	"fmt"
 	"net"
+
+	"github.com/scorify/schema"
 )
 
 type Schema struct {
 	Target string `key:"target"`
 	Port   int    `key:"port"`
+}
+
+func Validate(config string) error {
+	conf := Schema{}
+
+	err := schema.Unmarshal([]byte(config), &conf)
+	if err != nil {
+		return err
+	}
+
+	if conf.Target == "" {
+		return fmt.Errorf("target is required; got %q", conf.Target)
+	}
+
+	if conf.Port <= 0 || conf.Port > 65535 {
+		return fmt.Errorf("provided invalid port: %d", conf.Port)
+	}
+
+	return nil
 }
 
 func Run(ctx context.Context, config string) error {
